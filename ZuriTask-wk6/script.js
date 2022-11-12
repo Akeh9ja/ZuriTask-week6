057715400592c54903824755b867d2f2c53d6bda
Emmanuel Akeh <66576511+Akeh9ja@users.noreply.github.com>
2022-11-12 23:44:41 +0100

    finished adding comments

--- ZuriTask-wk6/script.js
+++ ZuriTask-wk6/script.js
@@ -1,8 +1,24 @@
-let butonIcons = document.getElementsByClassName('btn');
-let images = document.getElementsByTagName('img');
-
-//Mouse over effect
-images.addEventListener('click', e => {
-    butonIcons.target.style.opacity = 1;
-    console.log(butonIcons)
+//setting variables with datasets to make them nodelist
+let buttons = document.querySelectorAll('[data-carousel-button]');
+// looping through each button to make a function
+buttons.forEach(button => {
+  //adding a listener
+  button.addEventListener("click", () => {
+    //setting a rule to add through indexing
+    const clockwise = button.dataset.carouselButton === 'next' ? 1 : -1;
+    // referencing the parent container and the slides pictures
+    const slide = button.closest('[data-carousel]').querySelector('[data-slides]');
+    //getting the active slide
+    const activeslide = slide.querySelector('[data-active]');
+    // convertinng to array and changing the index of activeslide
+    let newIndex = [...slide.children].indexOf(activeslide) + clockwise;
+    //setting rules to start from the last and first
+    if (newIndex < 0) newIndex = slide.children.length - 1;
+    if (newIndex >= slide.children.length) newIndex = 0;
+    //changing the active role when conditions are met
+    slide.children[newIndex].dataset.active = true;
+    //deleting the previous active slide
+    delete activeslide.dataset.active;
+    
+  })
 });
